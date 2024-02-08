3c172c6cc8ad1b27630c6b25c9e14a4ac91fee0d
Alexander Genkin <alexander_genkin@gmx.net>
2024-02-08 22:05:33 +0200

    feat: read me

--- server/src/quote/quote.controller.spec.ts
+++ server/src/quote/quote.controller.spec.ts
@@ -11,23 +11,6 @@ import { HttpModule } from '@nestjs/axios';
 
 describe('QuotesController', () => {
   let app: INestApplication;
-  // let quotesService = { createQuote: () => {} };
-
-  // beforeAll(async () => {
-  //   const moduleRef: TestingModule = await Test.createTestingModule({
-  //     imports: [QuoteModule],
-  //     controllers: [QuoteController],
-  //     providers: [
-  //       {
-  //         provide: QuoteService,
-  //         useExisting: QuoteService,
-  //       },
-  //     ],
-  //   }).compile();
-  //
-  //   app = moduleRef.createNestApplication();
-  //   app.useGlobalPipes(new ValidationPipe());
-  //   await app.init();
   beforeAll(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
       imports: [
@@ -51,27 +34,27 @@ describe('QuotesController', () => {
   describe('calculateFinance DTO validation', () => {
     (() => {
       [
-        // {
-        //   text: '/POST create quote fails due to negative base amount',
-        //   base_amount: -1,
-        //   base_currency: 'BTC',
-        //   quote_currency: 'USD',
-        //   status: StatusCodes.BAD_REQUEST,
-        // },
-        // {
-        //   text: '/POST create quote fails due to wrong base currency',
-        //   base_amount: 1,
-        //   base_currency: 'BTC-ABC',
-        //   quote_currency: 'USD',
-        //   status: StatusCodes.BAD_REQUEST,
-        // },
-        // {
-        //   text: '/POST create quote fails due to negative quote currency',
-        //   base_amount: 1,
-        //   base_currency: 'BTC',
-        //   quote_currency: 'USD-BTC',
-        //   status: StatusCodes.BAD_REQUEST,
-        // },
+        {
+          text: '/POST create quote fails due to negative base amount',
+          base_amount: -1,
+          base_currency: 'BTC',
+          quote_currency: 'USD',
+          status: StatusCodes.BAD_REQUEST,
+        },
+        {
+          text: '/POST create quote fails due to wrong base currency',
+          base_amount: 1,
+          base_currency: 'BTC-ABC',
+          quote_currency: 'USD',
+          status: StatusCodes.BAD_REQUEST,
+        },
+        {
+          text: '/POST create quote fails due to negative quote currency',
+          base_amount: 1,
+          base_currency: 'BTC',
+          quote_currency: 'USD-BTC',
+          status: StatusCodes.BAD_REQUEST,
+        },
         {
           text: '/POST create quote works correctly with crypto currency',
           base_amount: 1,
@@ -86,13 +69,13 @@ describe('QuotesController', () => {
           quote_currency: 'BTC',
           status: StatusCodes.CREATED,
         },
-        // {
-        //   text: '/POST create quote fails due to same base and quote currency',
-        //   base_amount: 1,
-        //   base_currency: 'USD',
-        //   quote_currency: 'USD',
-        //   status: StatusCodes.BAD_REQUEST,
-        // },
+        {
+          text: '/POST create quote fails due to same base and quote currency',
+          base_amount: 1,
+          base_currency: 'USD',
+          quote_currency: 'USD',
+          status: StatusCodes.BAD_REQUEST,
+        },
       ].map(({ text, base_amount, base_currency, quote_currency, status }) => {
         it(text, async () => {
           const res = await request(app.getHttpServer())
