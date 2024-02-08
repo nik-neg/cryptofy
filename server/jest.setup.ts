8364b8ce3ed6ee44ebf46c328d1a425ae0a882d4
Alexander Genkin <alexander_genkin@gmx.net>
2024-02-08 22:03:22 +0200

    feat: read me

--- server/jest.setup.ts
+++ server/jest.setup.ts
@@ -2,7 +2,6 @@
 
 // Set environment variables for testing
 process.env.PORT = '3000';
-process.env.HOST = 'localhost';
 process.env.API_QUOTE_URL =
   'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest';
 process.env.API_KEY = 'your-api-key';
--- server/src/config/configuration.ts
+++ server/src/config/configuration.ts
@@ -1,5 +1,4 @@
 export default () => ({
-    port: parseInt(process.env.PORT, 10) || 3001,
-    api_key: process.env.API_KEY || 'api_key',
-    host: process.env.HOST || 'localhost',
+  port: parseInt(process.env.PORT, 10) || 3001,
+  api_key: process.env.API_KEY || 'api_key',
 });
