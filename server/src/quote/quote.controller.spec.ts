dadaed4b796c792daf764cae4593a0ae7713fdb6
Alexander Genkin <alexander_genkin@gmx.net>
2024-02-08 21:11:10 +0200

    test: dto, add exception

--- server/src/quote/quote.controller.spec.ts
+++ server/src/quote/quote.controller.spec.ts
@@ -3,6 +3,7 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { QuoteController } from './quote.controller';
 import { QuoteService } from './quote.service';
+import { StatusCodes } from 'http-status-codes';
 
 describe('QuotesController', () => {
   let app: INestApplication;
@@ -24,19 +25,64 @@ describe('QuotesController', () => {
     await app.init();
   });
 
-  it('/POST create quote', () => {
-    const createQuoteDto = {
-      base_amount: -1,
-      base_currency: 'BTC',
-      quote_currency: 'USD',
-    };
-    return request(app.getHttpServer())
-      .post('/quote')
-      .send(createQuoteDto)
-      .expect(201);
-  });
+  describe('calculateFinance DTO validation', () => {
+    (() => {
+      [
+        {
+          text: '/POST create quote fails due to negative base amount',
+          base_amount: -1,
+          base_currency: 'BTC',
+          quote_currency: 'USD',
+          status: StatusCodes.BAD_REQUEST,
+        },
+        {
+          text: '/POST create quote fails due to wrong base currency',
+          base_amount: 1,
+          base_currency: 'BTC-ABC',
+          quote_currency: 'USD',
+          status: StatusCodes.BAD_REQUEST,
+        },
+        {
+          text: '/POST create quote fails due to negative quote currency',
+          base_amount: 1,
+          base_currency: 'BTC',
+          quote_currency: 'USD-BTC',
+          status: StatusCodes.BAD_REQUEST,
+        },
+        {
+          text: '/POST create quote works correctly with crypto currency',
+          base_amount: 1,
+          base_currency: 'BTC',
+          quote_currency: 'USD',
+          status: StatusCodes.CREATED,
+        },
+        {
+          text: '/POST create quote works correctly with crypto currency',
+          base_amount: 1,
+          base_currency: 'USD',
+          quote_currency: 'BTC',
+          status: StatusCodes.CREATED,
+        },
+        {
+          text: '/POST create quote works correctly with crypto currency',
+          base_amount: 1,
+          base_currency: 'USD',
+          quote_currency: 'USD',
+          status: StatusCodes.BAD_REQUEST,
+        },
+      ].map(({ text, base_amount, base_currency, quote_currency, status }) => {
+        it(text, async () => {
+          const res = await request(app.getHttpServer())
+            .post('/quote')
+            .send({ base_amount, base_currency, quote_currency });
 
-  afterAll(async () => {
-    await app.close();
+          expect(res.status).toBe(status);
+        });
+      });
+    })();
+
+    afterEach(async () => {
+      await app.close();
+    });
   });
 });
--- server/src/quote/quote.controller.ts
+++ server/src/quote/quote.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { QuoteService } from './quote.service';
 import { CreateQuoteDto } from './dto/CreateQuoteDto.dto';
 import { QuoteResponse } from './types';
@@ -9,6 +9,11 @@ export class QuoteController {
 
   @Post()
   createQuote(@Body() createQuoteDto: CreateQuoteDto): Promise<QuoteResponse> {
+    if (createQuoteDto.base_currency === createQuoteDto.quote_currency) {
+      throw new BadRequestException(
+        'Base and quote currency cannot be the same',
+      );
+    }
     return this.quoteService.createQuote(createQuoteDto);
   }
 }
